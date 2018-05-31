316bf789b6fee84931977db9fdd479b73a83c3ff
Aulia <ahl7@163.com>
2018-05-31 19:24:48 +0800

    4

--- mylogapp/urls.py
+++ mylogapp/urls.py
@@ -4,5 +4,7 @@ from . import views
 app_name = 'mylogapp'
 urlpatterns = [
     path("", views.index, name='index'),
-    path("topics/", views.topics, name='topics')
+    path("topics/", views.topics, name='topics'),
+    path("topics/<int:topic_id>/", views.topic, name='topic'),
+    path("new_topic/", views.new_topic, name="new_topic")
 ]
--- mylogapp/views.py
+++ mylogapp/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from .forms import TopicForm
+from django.urls import reverse
 from .models import Topic
 # Create your views here.
 
@@ -11,3 +14,23 @@ def topics(request):
     topic = Topic.objects.order_by("date_added")
     content = {"topics": topic}
     return render(request, "mylogapp/topics.html", content)
+
+
+def topic(request, topic_id):
+    topic = Topic.objects.get(id=topic_id)
+    entries = topic.entry_set.order_by("-date_added")
+    context = {"topic":topic, "entries":entries}
+    return render(request, "mylogapp/topic.html", context)
+
+
+def new_topic(request):
+    if request.method != "POST":
+        form = TopicForm()
+    else:
+        form = TopicForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse("mylogapp:topics"))
+
+    context = {"form": form}
+    return render(request, "mylogapp/new_topic.html", context)
